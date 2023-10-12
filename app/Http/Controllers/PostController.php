48e963337f15ec70ba9cfc14dcef077b85e76eb7
Humberto Viveros <humberto.viverosrios@gmail.com>
2023-10-11 22:14:07 -0300

    Métodos index y destroy, recurso Post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,11 +5,26 @@ namespace App\Http\Controllers;
 // Illuminate hace referencia a clases propias de Laravel
 use Illuminate\Http\Request;
 
+// Importar modelo
+use App\Models\Post;
+
 class PostController extends Controller
 {
     // Configuramos los diferentes métodos de este recurso
     public function index()
     {
-        return view('posts.index');
+        // Retorna la vista, y un array con el contenido de la BD
+        // Con esto trae todos los registros, ahora se trabaja en la vista
+        return view('posts.index', [
+            'posts' => Post::latest()->paginate(),
+        ]);
+    }
+
+    public function destroy (Post $post)
+    {
+        // Al post que vino como parámetro, aplicar el método delete
+        $post->delete();
+        // Retornar a la lista de publicaciones
+        return back();
     }
 }
